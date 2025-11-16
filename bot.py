6ed307edf767103a7a13f2cbf72092ecfe2ccd4e
valnvmm <valnvmm1@gmail.com>
2025-11-16 22:06:40 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ async def on_ready():
 async def check(interaction: discord.Interaction):
     await interaction.response.send_message("I'm online.")
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("1417955841352532049")
 
 if not TOKEN:
     print("ERROR: No TOKEN environment variable found!")
