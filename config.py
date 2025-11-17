7bddf4e39c3ed0019421ae35a6e3e736f1ba599c
valnvmm <valnvmm1@gmail.com>
2025-11-17 20:53:32 +0100

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
 class Config:
-    TOKEN = os.getenv("DISCORD_TOKEN")                     # Discord Bot Token
-    DATABASE_URL = os.getenv("DATABASE_URL")       # PostgreSQL URL
+    TOKEN = os.getenv("DISCORD_TOKEN")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    if not TOKEN:
+        raise ValueError("❌ Missing TOKEN environment variable")
+
+    if not DATABASE_URL:
+        raise ValueError("❌ Missing DATABASE_URL environment variable")
